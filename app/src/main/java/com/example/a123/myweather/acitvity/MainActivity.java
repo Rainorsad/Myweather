2e98e0906c8c7c0087093611e4e0bd267a55b852
Rainorsad <457072858@qq.com>
2016-12-30 10:22:24 +0800

    Signed-off-by: Rainorsad <457072858@qq.com>

--- app/src/main/java/com/example/a123/myweather/acitvity/MainActivity.java
+++ app/src/main/java/com/example/a123/myweather/acitvity/MainActivity.java
@@ -1,12 +1,10 @@
 package com.example.a123.myweather.acitvity;
 
 import android.content.Context;
-import android.util.Log;
 import android.widget.TextView;
 
 import com.alibaba.fastjson.JSON;
 import com.example.a123.myweather.R;
-import com.example.a123.myweather.bean.WeatherBean;
 import com.example.a123.myweather.util.KJHttpUtils;
 
 import org.kymjs.kjframe.http.HttpCallBack;
@@ -41,7 +39,7 @@ public class MainActivity extends BaseActivity {
     @Override
     protected void setData() {
         setYearTime(); //设置年月时间
-        startGetData(); //开始访问网络请求数据
+        startGetData("通州","北京"); //开始访问网络请求数据
     }
 
     /**
@@ -62,9 +60,9 @@ public class MainActivity extends BaseActivity {
     /**
      * 开始访问网络请求数据
      */
-    private void startGetData() {
-        String city = toUtf("通州");
-        String privince = toUtf("北京");
+    private void startGetData(String c,String p) {
+        String city = toUtf(c);
+        String privince = toUtf(p);
         KJHttpUtils.getHttpWeather(context,city,privince,callBack);
     }
 
@@ -72,9 +70,9 @@ public class MainActivity extends BaseActivity {
         @Override
         public void onSuccess(String t) {
             if (JSON.parseObject(t).getInteger("retCode") == 200){
-                String s = String.valueOf(JSON.parseObject(t).getJSONObject("result"));
-                WeatherBean weatherBean = parseObject(s, WeatherBean.class);
-                Log.i(TAG, "onSuccess: "+weatherBean.toString());
+//                String s = String.valueOf(JSON.parseObject(t).getJSONObject("result"));
+//                WeatherBean weatherBean = parseObject(s, WeatherBean.class);
+//                Log.i(TAG, "onSuccess: "+weatherBean.toString());
             }else {
                 String msg = parseObject(t).getString("msg");
                 ToaS(context,msg);
--- app/src/main/java/com/example/a123/myweather/application/AppLica.java
+++ app/src/main/java/com/example/a123/myweather/application/AppLica.java
@@ -1,26 +1,14 @@
 package com.example.a123.myweather.application;
 
 import android.content.Context;
-
-import com.tencent.tinker.loader.app.TinkerApplication;
+import android.content.pm.ApplicationInfo;
 
 /**
  * Created by 123 on 2016/12/6.
  */
 
-public class AppLica extends TinkerApplication{
+public class AppLica extends ApplicationInfo{
     private Context context;
 
-    protected AppLica(int tinkerFlags, String delegateClassName, String loaderClassName, boolean tinkerLoadVerifyFlag) {
-        super(tinkerFlags, delegateClassName, loaderClassName, tinkerLoadVerifyFlag);
-    }
-
-    protected AppLica(int tinkerFlags) {
-        super(tinkerFlags);
-    }
-
-    protected AppLica(int tinkerFlags, String delegateClassName) {
-        super(tinkerFlags, delegateClassName);
-    }
 
 }
